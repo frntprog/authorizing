7553200a1a1469bd73b2e199d713415746f118f4
Melnyk Denys <dmelnyk@exadel.net>
2021-04-29 16:09:37 +0300

    added log in and registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,32 +1,111 @@
 const User = require('../models/User');
 const Role = require('../models/Role');
+const bcrypt = require('bcryptjs');
+const {
+    validationResult
+} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const {
+    secret
+} = require('../config');
+
+const generateAccessToken = (id, role) => {
+    const payload = {
+        id,
+        role
+    }
+    return jwt.sign(payload, secret, {
+        expiresIn: "30min"
+    })
+}
 
 class authController {
-    async registration(req, res){
-        try{
-
-        }catch(e){
-            
+    async registration(req, res) {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    message: "Error while registration",
+                    errors
+                })
+            }
+            const {
+                username,
+                password
+            } = req.body;
+            const candidate = await User.findOne({
+                username
+            });
+            if (candidate) {
+                return res.status(400).json({
+                    message: "The user with such username already exists."
+                })
+            }
+            const hashPassword = bcrypt.hashSync(password, 7);
+            const userRole = await Role.findOne({
+                value: "ADMIN"
+            });
+            const user = new User({
+                username,
+                password: hashPassword,
+                roles: [userRole.value]
+            });
+            await user.save();
+            return res.json({
+                message: "The user was registrated"
+            });
+        } catch (e) {
+            console.log(e);
+            res.status(400).json({
+                message: "Registration error!"
+            });
         }
     }
 
-    async login(req, res){
-        try{
+    async login(req, res) {
+        try {
+            const {
+                username,
+                password
+            } = req.body;
+            const user = await User.findOne({
+                username
+            });
+            if (!user) {
+                return res.status(400).json({
+                    message: `The user ${username} is not found`
+                });
+            }
 
-        }catch(e){
-            
+            const validPassword = bcrypt.compareSync(password, user.password);
+            if (!validPassword) {
+                return res.status(400).json({
+                    message: `The password is invalid`
+                });
+            }
+            const token = generateAccessToken(user._id, user.roles);
+
+            return res.json({
+                token
+            });
+        } catch (e) {
+            console.log(e);
+            res.status(400).json({
+                message: "Registration error!"
+            });
         }
     }
 
-    async getUsers(req, res){
-        try{
-            const userRole = new Role();
-            const adminRole = new Role({value: "ADMIN"});
-            await userRole.save();
-            await adminRole.save();
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users)
             res.json("Server works!!!")
-        }catch(e){
-
+        } catch (e) {
+            console.log(e);
+            res.status(400).json({
+                message: "Registration error!"
+            });
         }
     }
 }
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,16 @@
 const Router = require('express');
 const router = new Router();
 const controller = require("../controllers/authController");
+const {check} = require('express-validator');
+const authMiddleware = require("../middlewares/authMiddleware");
 
-router.post('/registration', controller.registration);
+router.post('/registration', [
+    check('username', "The username can't be empty!").notEmpty(),
+    check('password', "The password must be bigger that 4 and less than 10 characters").isLength(4, 10)
+], controller.registration);
+
 router.post('/login', controller.login);
-router.get('/users', controller.getUsers);
+
+router.get('/users', authMiddleware, controller.getUsers);
 
 module.exports = router;
